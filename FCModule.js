9e0f87bed9732fa9b38ceb9512eedc8fab5ffeef
RayTCosgrove <RayTCosgrove@gmail.com>
2020-08-03 17:18:31 -0400

    Add files via upload

---
+++ FCModule.js
@@ -0,0 +1,39 @@
+//helloworld.js:
+
+Module.register("FCMODULE",{
+	// Default module config.
+	defaults: {
+		text: "FC!"
+	},
+
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.id = 'FC'
+		wrapper.innerHTML = this.config.text;
+
+
+		return wrapper;
+	},
+
+	start: function() {
+	this.mySpecialProperty = "So much wow!";
+	Log.log(this.name + ' is started!');
+
+	const evtSource = new EventSource("192.168.86.248:3000/test")
+		evtSource.onmessage = function(e){
+		Log.log(e.data)
+		var node = document.createElement('SPAN')
+		var text = document.createTextNode(JSON.parse(e.data).timestamp)
+		node.appendChild(text)
+		var x = document.getElementById('FC')
+		x.appendChild(node)
+		}
+	
+},
+
+  notificationReceived: function() {},
+  socketNotificationReceived: function() {},
+
+});
